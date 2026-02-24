db1ac3d467bb0a100e4a9e0b10e791f17f0ca0fb
sakamoto-jun <sakamotojun.dev@gmail.com>
2026-02-25 05:48:59 +0900

    fix: React Query 잔여 코드 제거

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,15 +1,7 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useState } from "react";
-import { ThemeProvider } from "@/contexts/ThemeContext";
+import { ThemeProvider } from '@/contexts/ThemeContext';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider>{children}</ThemeProvider>
-    </QueryClientProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
